a721fb0ab9bb21e621b8895cd5a6d8a22a02b004
Kyle Porter <kyleporter@Kyles-Air.attlocal.net>
2019-12-18 14:50:09 -0500

    added router for app

--- friend-app/src/App.js
+++ friend-app/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <nav>
+        <Link to='/login' >Sign In</Link>
+        <Link to='/FriendsList' >Friends</Link>
+      </nav>
+
+      <PrivateRoute path="/FriendsList" component={FriendsList} />
+      <Route path="/login" component={Login} />
     </div>
   );
 }
